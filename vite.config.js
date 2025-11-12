d1e14dc348d52449e3ffd78314099269becda316
eduardpinte52 <pintee00@gmail.com>
2025-11-12 10:14:24 +0200

    Modificari rute

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
+  base: '/MyProject/',
+  plugins: [
+    vue(),
+    vueDevTools()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
